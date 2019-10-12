2cae2019573363e0ce1de5bf09a05104c3853cea
Frank Meza <frankmezapdx@gmail.com>
2019-10-12 15:01:43 -0700

    working todo app

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,19 @@
 <!-- JAVASCRIPT LOGIC -->
 <script lang="ts">
+    const NEW_ITEM = "Adding a new item: "
+    const SUBMISSION = "submit new item"
+
+    const handleSubmit = () => {
+        todoItems = [...todoItems, newItem]
+        newItem = ""
+    }
+
     let todoItems: ReadonlyArray<string> = [
         "go for a walk",
         "write a todo app using svelte",
     ]
+
+    let newItem: string = ""
 </script>
 
 <!-- HTML MARKUP -->
@@ -14,6 +24,10 @@
         <li>{item}</li>
     {/each}
 </ul>
+
+<h3>{NEW_ITEM} {newItem}</h3>
+<textarea bind:value={newItem}></textarea>
+<button on:click={handleSubmit}>{SUBMISSION}</button>
 
 <!-- CSS STYLES -->
 <style>
