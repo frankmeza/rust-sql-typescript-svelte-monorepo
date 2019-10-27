ae05ad98449696b4b26cf813a29b2ed990129a57
Frank Meza <frankmezapdx@gmail.com>
2019-10-27 15:13:25 -0700

    show fetched items

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,3 @@
-<!-- JAVASCRIPT LOGIC -->
 <script lang="ts">
     let NEW_ITEM = "Adding a new item: "
     let SUBMISSION = "submit new item"
@@ -11,38 +10,41 @@
 
     let todoItems: ReadonlyArray<string> = [
         "go for a walk",
-        "write a todo app using svelte",
+        "write a todo app using svelte"
     ]
 
     let newItem: string = ""
-
-    let list: string = ""
+    let people: string = ""
 
     async function getFromServer() {
         let res = await fetch("http://localhost:8088/again")
         let json = await res.json()
 
-        list = json
+        people = json
     }
 </script>
 
+<style>
+
+</style>
+
+<!-- JAVASCRIPT LOGIC -->
 <!-- HTML MARKUP -->
 <h1>Todo App</h1>
 
 <ul>
-    {#each todoItems as item}
-        <li>{item}</li>
-    {/each}
+  {#each todoItems as item}
+    <li>{item}</li>
+  {/each}
 </ul>
 
-<p>{JSON.stringify(list)}</p>
+<ul>
+  {#each people as person}
+    <li>{person.name}</li>
+  {/each}
+</ul>
+<p>{JSON.stringify(people)}</p>
 
 <h3>{NEW_ITEM} {newItem}</h3>
-<textarea bind:value={newItem}></textarea>
+<textarea bind:value={newItem} />
 <button on:click={handleSubmit}>{SUBMISSION}</button>
-
-<!-- <p>{list}</p> -->
-
-<!-- CSS STYLES -->
-<style>
-</style>
