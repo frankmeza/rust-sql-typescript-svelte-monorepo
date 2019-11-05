5ce267d0fee53378267234a61daf63cfe0e2e63a
Frank Meza <frankmezapdx@gmail.com>
2019-11-04 21:51:50 -0800

    fetch and show all people list

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
     import { Person } from "./core"
-    import { getPeople } from "./app_utils"
+    import { getPeople } from "./async_app_utils"
 
+    // constants
+    const PEOPLE_DATABASE = "People Database"
+    const BUTTON_TEXT = "GET /people"
+
+    // variables
     let people: Array<Person> = []
 
+    // async handlers
     const handleClick = async () => {
         people = await getPeople()
     }
+
+    // sync utils
+    const formatListItem = (p: Person): string => {
+        return `{ id: ${p.id}, name: ${p.name} }`
+    }
 </script>
 
-<h1>People Database</h1>
+<h1>{PEOPLE_DATABASE}</h1>
 
-<button on:click={handleClick}>GET /people</button>
+<button on:click={handleClick}>{BUTTON_TEXT}</button>
 
-<ul>
-  {#each people as person}
-    <li> id: {person.id}, name: {person.name}</li>
-  {/each}
-</ul>
+{#if people.length === 0}
+    <p>ain't nobody here yet</p>
+{/if}
 
-<style></style>
+{#if people.length > 0}
+    <ul>
+    {#each people as person}
+        <li>{formatListItem(person)}</li>
+    {/each}
+    </ul>
+{/if}
+
+<style>
+    h1 {
+        color: brown;
+        font-family: Hasklig;
+    }
+</style>
