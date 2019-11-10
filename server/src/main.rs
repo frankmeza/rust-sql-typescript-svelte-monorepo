9f6bad7c2d4bcb81eb6b636b4ec803d9fd12c00d
Frank Meza <frankmezapdx@gmail.com>
2019-11-09 19:00:53 -0800

    server serves ws and http, some clean up

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,39 +16,36 @@ pub fn get_connection() -> Connection {
         .expect("wut happen postgres")
 }
 
-fn start_ws() {
-    ws_server::start();
-}
-
 fn main() {
     env_logger::init();
 
-    println!("Server did something");
-    start_ws();
+    println!("Server started");
+    // start_ws();
 
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .wrap(
-    //             Cors::new()
-    //                 .allowed_origin("http://localhost:10001")
-    //                 .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-    //                 .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-    //                 .allowed_header(header::CONTENT_TYPE)
-    //                 .max_age(3600),
-    //         )
-    //         .route("/people", web::get().to(responders::get_people_list))
-    //         .route("/people", web::post().to(responders::create_person))
-    //         .route("/people", web::put().to(responders::update_person_by_id))
-    //         .route("/people/{id}", web::get().to(responders::get_person_by_id))
-    //         .route(
-    //             "/people/{id}",
-    //             web::delete().to(responders::delete_person_by_id),
-    //         )
-    // })
-    // .bind("127.0.0.1:8088")
-    // .unwrap()
-    // .run()
-    // .unwrap();
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::new()
+                    .allowed_origin("http://localhost:10001")
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600),
+            )
+            .route("/ws/", web::get().to(ws_server::start))
+            .route("/people", web::get().to(responders::get_people_list))
+            .route("/people", web::post().to(responders::create_person))
+            .route("/people", web::put().to(responders::update_person_by_id))
+            .route("/people/{id}", web::get().to(responders::get_person_by_id))
+            .route(
+                "/people/{id}",
+                web::delete().to(responders::delete_person_by_id),
+            )
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
 
     println!("Server got here");
     println!("Server started");
--- server/src/ws_server/mod.rs
+++ server/src/ws_server/mod.rs
@@ -1,5 +1,5 @@
 use actix::{Actor, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
 /// Define http actor
@@ -21,36 +21,8 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for MyWs {
     }
 }
 
-fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+pub fn start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     let resp = ws::start(MyWs {}, &req, stream);
     println!("{:?}", resp);
     resp
 }
-
-pub fn start() {
-    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run()
-        .unwrap();
-}
-
-// use ws::listen;
-
-// pub fn start() {
-//     // Listen on an address and call the closure for each connection
-//     println!("Server got here");
-//     if let Err(error) = listen("127.0.0.1:8888", |out| {
-//         // The handler needs to take ownership of out, so we use move
-//         move |msg| {
-//             // Handle messages received on this connection
-//             println!("Server got message '{}'. ", msg);
-
-//             // Use the out channel to send messages back
-//             out.send(msg)
-//         }
-//     }) {
-//         // Inform the user of failure
-//         println!("Failed to create WebSocket due to {:?}", error);
-//     }
-// }
