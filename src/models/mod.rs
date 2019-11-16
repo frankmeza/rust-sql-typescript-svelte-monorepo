b3915672879aef33cdfb8c542a9ad5f523ce8034
Frank Meza <frankmezapdx@gmail.com>
2019-11-16 15:53:23 -0800

    add request struct

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,12 @@
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct PersonReq {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Person {
-    pub id: i32,
+    pub id: String,
     pub name: String,
 }
