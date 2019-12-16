f586784b2e48fdad454fcd7c248bccba24e09003
Frank Meza <frankmezapdx@gmail.com>
2019-12-15 23:03:30 -0800

    just being a DRY nerd, add typings

--- client/src/utils/app_utils.ts
+++ client/src/utils/app_utils.ts
@@ -4,9 +4,19 @@ const BASE_URL = "http://localhost:8088"
 const headers = { "Content-Type": "application/json" }
 
 type RequestMethod = "POST" | "PUT" | "DELETE"
-type RequestBody = {}
 
-const buildRequest = (method: RequestMethod, body: RequestBody) => ({
+interface RequestBody {
+    readonly id: string
+    readonly name: string
+}
+
+interface AppRequest {
+    readonly body: string
+    readonly headers: {}
+    readonly method: RequestMethod
+}
+
+const buildRequest = (method: RequestMethod, body: Partial<RequestBody>): AppRequest => ({
 	body: JSON.stringify(body),
 	headers,
 	method,
@@ -14,7 +24,7 @@ const buildRequest = (method: RequestMethod, body: RequestBody) => ({
 
 // TODO add error handling
 export async function getPeople(): Promise<Person[]> {
-	const response = await fetch(`${BASE_URL}/people`)
+	const response: Response = await fetch(`${BASE_URL}/people`)
 	const people: Person[] = await response.json()
 
 	return people
@@ -22,38 +32,49 @@ export async function getPeople(): Promise<Person[]> {
 
 // TODO add error handling
 export async function getPersonById(id: string): Promise<Person> {
-	const reqBody = { id }
-	const request = buildRequest("POST", reqBody)
+	const reqBody: Partial<RequestBody> = { id }
 
+	const request = buildRequest("POST", reqBody)
 	const response = await fetch(`${BASE_URL}/people/${id}`, request)
 	const person: Person = await response.json()
 
 	return person
 }
 
+const statusReturnHelper = (rs: Response): string => {
+    if (rs.ok) {
+        console.log(`${name} was created 👍`)
+        return "ok"
+    } else {
+        console.log(`things is bad very virus, look ${rs}`)
+        return rs.statusText
+    }
+}
+
 export async function createPerson(name: string): Promise<string> {
-    const reqBody = ({ name })
+    const reqBody: Partial<RequestBody> = ({ name })
 
     const request = buildRequest("POST", reqBody)
     const response = await fetch(`${BASE_URL}/people`, request)
 
-    return response.ok ? "ok" : response.statusText
+    return statusReturnHelper(response)
 }
 
 export async function updatePersonById(person: Person): Promise<string> {
 	const { id, name } = person
-	const reqBody = JSON.stringify({ id, name })
+	const reqBody: Partial<RequestBody> = ({ id, name })
 
 	const request = buildRequest("PUT", reqBody)
-	const response = await fetch(`${BASE_URL}/people`, request)
+    const response = await fetch(`${BASE_URL}/people`, request)
 
-	return response.ok ? "ok" : response.statusText
+    return statusReturnHelper(response)
 }
 
 export async function deletePersonById(id: string): Promise<string> {
-	const reqBody = { id }
-	const request = buildRequest("DELETE", reqBody)
+	const reqBody: Partial<RequestBody>  = { id }
+
+    const request = buildRequest("DELETE", reqBody)
 	const response = await fetch(`${BASE_URL}/people`, request)
 
-	return response.ok ? "ok" : response.statusText
+    return statusReturnHelper(response)
 }
