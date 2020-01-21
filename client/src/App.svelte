b38592fc7347f06180cae77dcd1136c4ac5ba432
Frank Meza <frankmezapdx@gmail.com>
2020-01-20 22:46:34 -0800

    fix imports, and i think everything else 🤷‍♂️👍

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import AddPerson from "./add_person.svelte"
-    import DeletePerson from "./delete_person.svelte"
-    import UpdatePerson from "./update_person.svelte"
-    import GetPeople from "./get_people.svelte"
+    import AddPerson from "./people/add_person.svelte"
+    import DeletePerson from "./people/delete_person.svelte"
+    import UpdatePerson from "./people/update_person.svelte"
+    import GetPeople from "./people/get_people.svelte"
 </script>
 
 <AddPerson />
